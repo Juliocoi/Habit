133a3324011c7298862e369fa12d932ee7103cc1
JulioCoi <amorim.jc@gmail.com>
2023-07-18 15:46:12 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Júlio César Amorim on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
